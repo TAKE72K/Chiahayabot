0838e56c300ca6068101262bf1c12911ede19367
TAKE72K <hyes941073@gmail.com>
2018-07-20 12:43:06 +0800

    a

--- 72botpub.py
+++ 72botpub.py
@@ -1,11 +1,23 @@
 ﻿import telegram
+import re
+import random
 from datetime import datetime,time, tzinfo, timedelta
 from telegram import InlineQueryResultArticle, InputTextMessageContent,InlineKeyboardMarkup,InlineKeyboardButton
 from telegram.ext import Updater,CommandHandler,MessageHandler,Filters,InlineQueryHandler,JobQueue
 '''not sure if need to import'''
 #遊戲部ID:-1001232423456
+'''
+command list
+start-名為72的偶像
+help-72能做什麼?
+time-現在幾點
+gdmn-早安
+kenka-吵架
+grave-擔當太尊而猝死的P用
+c-test function count members
+'''
 
-updater=Updater(token='TOKEN')
+updater=Updater(token='')
 
 dispatcher = updater.dispatcher
 import logging
@@ -22,7 +34,28 @@ def build_menu(buttons,
         menu.append(footer_buttons)
     return menu
 
-	
+HALF2FULL = dict((i, i + 0xFEE0) for i in range(0x21, 0x7F))
+HALF2FULL[0x20] = 0x3000
+def fullen(s):
+    '''
+    Convert all ASCII characters to the full-width counterpart.
+    '''
+    return str(s).translate(HALF2FULL)
+'''
+def HalfToFull(s):
+	n = []
+	s = s.decode('utf-8')
+	for char in s:
+		num = ord(char)
+		if num == 0x3000:
+			num = 32
+		elif 0xFF01 <= num <= 0xFF5E:
+			num -= 0xfee0 
+		num = unichr(num)
+		n.append(num)
+		
+	return ''.join(n)
+'''	
 	
 class GMT8(tzinfo):
 	def utcoffset(self, dt):
@@ -46,18 +79,75 @@ job_m=daily_remind.run_daily(callback11,t)
 def start(bot, update):
 	bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
 	bot.send_message(chat_id=update.message.chat_id, text="如月千早です。劇場という場所があることは、レッスンの励みにもなりますね。これからも、厳しいご指導をよろしくお願いします。")
+	bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+	bot.send_message(chat_id=update.message.chat_id, text='私のコマンドリストです：\n/start-名為72的偶像\n/help-72能做什麼?\n/time-現在幾點\n/gdmn-早安\n/kenka-吵架\n/grave-擔當太尊而猝死的P用\n/c-test function count members')
 	
 	button_list=[
-		InlineKeyboardButton(text='start',switch_inline_query='Chiahayabot',switch_inline_current_chat='/start'),
+		InlineKeyboardButton(text='start',switch_inline_query='/start',switch_inline_current_chat='/start'),
 		InlineKeyboardButton(text='about 72',url='https://imasml-theater-wiki.gamerch.com/%E5%A6%82%E6%9C%88%E5%8D%83%E6%97%A9#content_2_13')
 		]
 	reply_markup = InlineKeyboardMarkup(build_menu(button_list, n_cols=1))
 	bot.send_message(chat_id=update.message.chat_id, text="A menu test", reply_markup=reply_markup)
+	
 	'''test of inline button'''
+def help(bot,update):
+	bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+	bot.send_message(chat_id=update.message.chat_id, text='私のコマンドリストです：\n/start-名為72的偶像\n/help-72能做什麼?\n/time-現在幾點\n/gdmn-早安\n/kenka-吵架\n/grave-擔當太尊而猝死的P用\n/c-test function count members')
+	
 
-
-start_handler = CommandHandler(['start','help'], start)
+start_handler = CommandHandler('start', start)
+help_handler = CommandHandler('help', help)
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(help_handler)
+
+def invite(bot,update):
+	bot.send_message(chat_id=update.message.chat_id, text='加入阿克西斯教，just now')
+	bot.export_chat_invite_link(chat_id=update.message.chat_id)
+invite_handler=CommandHandler('linkstart',invite)
+dispatcher.add_handler(invite_handler)
+
+def title(bot,update,args):
+	title = ' '.join(args)
+	adminlist=update.message.chat.get_administrators()
+	is_admin=False
+	
+	me=bot.get_me()
+	bot_auth=False
+	for i in adminlist:
+		if update.message.from_user.id==i.user.id:
+			is_admin=True
+	for b in adminlist:
+			if me.id==b.user.id:
+				bot_auth=True
+	if is_admin==True:
+		if bot_auth==True:
+			bot.set_chat_title(chat_id=update.message.chat_id, title=title)
+			bot.send_message(chat_id=update.message.chat_id,text='title changed')
+		else:
+			bot.send_message(chat_id=update.message.chat_id,text='Bot:Not enough rights to change chat title')
+		
+		
+	else:
+		test='list:your id'+str(update.message.from_user.id)+'\n'+'admin id:'
+		for i in adminlist:
+			test=test+str(i.user.id)+'\n'
+		bot.send_message(chat_id=update.message.chat_id,text=test)
+		bot.send_message(chat_id=update.message.chat_id,text='User:Not enough rights to change chat title')
+		if bot_auth==True:
+			
+			bot.send_message(chat_id=update.message.chat_id,text='Bot:Auth comfirm')
+		else:
+			bot.send_message(chat_id=update.message.chat_id,text='Bot:Not enough rights to change chat title')
+title_handler=CommandHandler('title',title,pass_args=True)
+dispatcher.add_handler(title_handler)
+
+
+
+def stk(bot,update):
+	if update.message.sticker!=None:
+		bot.send_message(chat_id=update.message.chat_id,text=update.message.sticker.file_id)
+stk_handler=CommandHandler('stk',stk)
+dispatcher.add_handler(stk_handler)
 
 def gdmn(bot,update):
 	#a good morning func
@@ -79,16 +169,138 @@ def gdmn(bot,update):
 	bot.send_message(chat_id=update.message.chat_id,text=text)
 	
 	
-	custom_keyboard = [['/start', '/gdmn'], ['72', '找飯店'],['そらそら']]
+	#custom_keyboard = [['/start', '/gdmn'], ['72', '找飯店'],['そらそら']]
 	
-	reply_markup = telegram.ReplyKeyboardMarkup(keyboard=custom_keyboard,one_time_keyboard=True)#one_time_kb:initial false ,dissapear after touch once
+	#reply_markup = telegram.ReplyKeyboardMarkup(keyboard=custom_keyboard,one_time_keyboard=True)#one_time_kb:initial false ,dissapear after touch once
 	
-	bot.send_message(chat_id=chat_id, text="KeyBoard test~~", reply_markup=reply_markup)
+	#bot.send_message(chat_id=chat_id, text="KeyBoard test~~", reply_markup=reply_markup)
 
 
 	
 gdmn_handler=CommandHandler('gdmn',gdmn)
 dispatcher.add_handler(gdmn_handler)
+def count(bot,update):
+	time = datetime.now().strftime("%H:%M:%S")
+	time='現在時間:'+time
+	count=bot.get_chat_members_count(chat_id=update.message.chat.id)
+	text='室內人數'+str(count)+'\n'+time
+	bot.send_message(chat_id=update.message.chat_id,text=text)
+	
+count_handler=CommandHandler('c',count)
+dispatcher.add_handler(count_handler)
+
+def grave(bot,update):
+	par=random.randint(0,1)
+	if par==0:
+		text=('　　　／￣￣〈￣￣＼ :.＼ \n'
+			'　　 |　　　　　　　　|: ::| \n'
+			'　 　|　$pname|: ::| \n'
+			'　 　|　$s2|: ::| \n'
+			'　 　|　$s3|: ::| \n'
+			'　 　|　　　　　　　　|: ::| \n'
+			'　 　|　　安息於此　　|: ::| \n'
+			'　 　|　　　　　　　　|: ::| \n'
+			'￣\"￣\"￣\'￣￣\"\"￣\"\"￣.￣\'￣￣\n '
+			)
+		pname=update.message.from_user.first_name
+		pname=fullen(pname)
+		op=pname
+		l=len(pname)
+		if len(pname)<7:
+			
+			for l in range(l,7):
+				op+='　'
+			text=text.replace('$pname',op)
+			text=text.replace('$s2','　　　　　　　')
+			text=text.replace('$s3','　　　　　　　')
+		if len(pname)==7:
+			text=text.replace('$pname',op)
+			text=text.replace('$s2','　　　　　　　')
+			text=text.replace('$s3','　　　　　　　')
+		if len(pname)>7:
+			plist=re.findall(r'\w{1,7}',pname)
+			if l>7 and l<15:
+				l2=len(plist[1])
+				for l2 in range(l2,7):
+					plist[1]+='　'
+				text=text.replace('$pname',plist[0])
+				text=text.replace('$s2',plist[1])
+				text=text.replace('$s3','　　　　　　　')
+			if l>14:
+				l2=len(plist[1])
+				
+				l3=len(plist[2])
+				for l3 in range(l3,7):
+					plist[2]+='　'
+				text=text.replace('$pname',plist[0])
+				text=text.replace('$s2',plist[1])
+				text=text.replace('$s3',plist[2])
+				
+				
+			
+		bot.send_message(chat_id=update.message.chat_id,text=text)
+	if par>0:
+		top='　　 ＿ \n'
+		nak='　　|$name| \n'
+		bas=('　|￣￣￣| \n'
+			'　| |三三| | \n'
+			'￣￣￣￣￣￣ \n')
+		op=top
+		pname=update.message.from_user.first_name
+		pname=fullen(pname)
+		l=len(pname)
+		plist=re.findall(r'\w{1,1}',pname)
+		for a in range(0,l):
+			
+			t=nak.replace('$name',plist[a])
+			op+=t
+		op+=bas
+		bot.send_message(chat_id=update.message.chat_id,text=op)
+		
+		
+		
+grave_handler=CommandHandler('grave',grave)
+dispatcher.add_handler(grave_handler)
+
+'''
+　　　／￣￣〈￣￣＼ :.＼ 
+　　 |　　　　　　　　|: ::| 
+　 　|　他給Ｐ　　　　|: ::| 
+　 　|　　　　　　　　|: ::| 
+　 　|　takep~~ 　　　|: ::| 
+　 　|　　　　　　　　|: ::| 
+　 　|　　　　　　　　|: ::| 
+　 　|　　　　　　　　|: ::| 
+￣"￣"￣'￣￣""￣""￣.￣'￣￣ 
+
+
+
+'''
+
+
+'''
+　　 ＿ 
+　　|ク| 
+　　|ナ| 
+　　|ウ| 
+　　|ド| 
+　　|＠| 
+　　|蝉| 
+　|￣￣￣| 
+　| |三三| | 
+￣￣￣￣￣￣ 
+
+
+
+'''
+
+
+
+
+
+
+
+
 
 
 
@@ -101,29 +313,91 @@ def tis(bot,update):
 tis_handler=CommandHandler('time',tis)
 dispatcher.add_handler(tis_handler)
 
+def kenka(bot,update):
+	text='$888 你要我打誰？'
+	text=text.replace("$888",str(update.message.from_user.first_name))
+	bot.send_message(chat_id=update.message.chat_id,text=text)
+	
+	
+kenka_handler=CommandHandler('kenka',kenka)
+dispatcher.add_handler(kenka_handler)
+
+def punch(bot,update,args):
+	bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+	text = ' '.join(args)+'吃我木蘭飛彈ㄅ'
+	bot.send_message(chat_id=update.message.chat_id, text=text)
+	
+punch_handler = CommandHandler('punch', punch, pass_args=True)
+dispatcher.add_handler(punch_handler)
+def caps(bot, update, args):
+	bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+	text_caps = ' '.join(args).upper()
+	bot.send_message(chat_id=update.message.chat_id, text=text_caps)
+
+caps_handler = CommandHandler('caps', caps, pass_args=True)
+dispatcher.add_handler(caps_handler)
 
 
 
+def inline_ku(bot,update):
+	query=update.inline_query.query+"?\nくっ......"
+	
+	if not query:
+		return
+	results = list()
+	result=InlineQueryResultArticle(
+		id=query.upper(),
+		title='KU',
+		input_message_content=InputTextMessageContent(query)
+		)
+	bot.answer_inline_query(update.inline_query.id, results)
+
+inline_ku_handler = InlineQueryHandler(inline_ku)
+dispatcher.add_handler(inline_ku_handler)
 
 def sora(bot,update):
 	#an filter handler
 	#predict to be unable if privacy mode is on(st bot can't heard text filter real time)
-	test=update.message.text
-	if test=="そらそら":
-		bot.send_message(chat_id=update.message.chat_id, text="我愛そらそら")
-		
-	elif test=="飯店" or test=="找飯店":
-		bot.send_message(chat_id=update.message.chat_id, text="TRIVAGO!!!!!!!!!!")
-	elif test=="72" :
-		bot.send_message(chat_id=update.message.chat_id, text="939393939393939393!!!!!!!!!!")
+	test=str(update.message.text)
+	
+	if test.find('我也愛そらそら')!=-1:
+		bot.send_message(chat_id=update.message.chat_id, text="我愛そらそら一生一世")
+	elif test.find('我愛そらそら')!=-1:
+		bot.send_message(chat_id=update.message.chat_id, text="我愛そらそら一生一世")
+	elif test.find('そらそら')!=-1:
+		bot.send_message(chat_id=update.message.chat_id,text="我愛そらそら")
+	
+	elif test.find("72")!=-1:
 		bot.send_sticker(chat_id=update.message.chat_id, sticker="CAADBQAD5gQAAsZRxhVjgK6PcwABUaUC")
+		#bot.send_message(chat_id=update.message.chat_id, text="媽的，72是有錯一樣！一直7272不會煩嗎？")
+		#bot.leave_chat(chat_id=update.message.chat_id)
+		#bot.send_message(chat_id=update.message.chat_id, text="939393939393939393!!!!!!!!!!")
+		
+	elif test.find("飯店")!=-1:
+		return
+		bot.send_message(chat_id=update.message.chat_id, text="TRIVAGO!!!!!!!!!!")
 	elif test=='下班':
+		return
 		time = datetime.now().strftime("%H:%M:%S")
 		time='現在時間:'+time+'\n下班囉XDD'
 		#datetime.datetime.now()
 		bot.send_message(chat_id=update.message.chat_id,text=time)
 	else:
-		bot.send_sticker(chat_id=update.message.chat_id, sticker="CAADBQAD5gQAAsZRxhVjgK6PcwABUaUC")
+		#bot.send_message(chat_id='', text="不要玩弄我ㄉ感情")
+		#bot.send_message(chat_id='', text=str(update.message.date))
+		if update.message.new_chat_members!=None:
+			new_chat_members=update.message.new_chat_members
+			for u in new_chat_members:
+				text='野生的'+u.first_name+'出現了'
+				bot.send_message(chat_id=update.message.chat_id,text=text)
+		if update.message.left_chat_member!=None:
+			text=update.message.left_chat_member.first_name+'，6666666666！'
+			bot.send_message(chat_id=update.message.chat_id,text=text)
+		if update.message.sticker!=None and update.message.chat.id==-313454366:
+			bot.send_message(chat_id=-313454366,text=update.message.sticker.file_id)
+		
+			
+		#bot.send_sticker(chat_id=update.message.chat_id, sticker="CAADBQAD5gQAAsZRxhVjgK6PcwABUaUC")#CAADBQAD5gQAAsZRxhVjgK6PcwABUaUC
 
 #Method: channels.inviteToChannel
 #Result: {"_":"rpc_error","error_code":400,"error_message":"USER_KICKED"}
@@ -147,30 +421,6 @@ dispatcher.add_handler(trivago_handler)'''
 echo_handler = MessageHandler(Filters.text, echo)
 dispatcher.add_handler(echo_handler)'''
 
-def caps(bot, update, args):
-	bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
-	text_caps = ' '.join(args).upper()
-	bot.send_message(chat_id=update.message.chat_id, text=text_caps)
-
-caps_handler = CommandHandler('caps', caps, pass_args=True)
-dispatcher.add_handler(caps_handler)
-
-
-def inline_ku(bot,update):
-	query=update.inline_query.query+"?\nくっ......"
-	
-	if not query:
-		return
-	results = list()
-	result=InlineQueryResultArticle(
-		id=query.upper(),
-		title='KU',
-		input_message_content=InputTextMessageContent(query)
-		)
-	bot.answer_inline_query(update.inline_query.id, results)
-
-inline_ku_handler = InlineQueryHandler(inline_ku)
-dispatcher.add_handler(inline_ku_handler)
 
 def unknown(bot, update):
 	bot.send_message(chat_id=update.message.chat_id, text="すみません、よく分かりません。")
