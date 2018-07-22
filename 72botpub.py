7ea27fe49768802f90e749dd686760ba54846d43
TAKE72K <hyes941073@gmail.com>
2018-07-22 18:41:04 +0800

    g

--- 72botpub.py
+++ 72botpub.py
@@ -209,7 +209,7 @@ def gdmn(bot,update):
     message=update.message
     chat_id = message.chat.id
     bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
-	nsheet=sheet.worksheet('name')
+    nsheet=sheet.worksheet('name')
     #replace $username
     try:
         cellid=nsheet.find(str(update.message.from_user.id))
