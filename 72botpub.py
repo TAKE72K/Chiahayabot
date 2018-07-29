cf2447110377f97da0e1c790c55673dcf1c54967
TAKE72K <hyes941073@gmail.com>
2018-07-29 21:47:02 +0800

    bot

--- 72botpub.py
+++ 72botpub.py
@@ -574,13 +574,13 @@ def sora(bot,update):
     test=str(update.message.text)
     
     if test.find(' #名言')!=-1 or test.find('#名言 ')!=-1:
-        if update.message.reply_to_message==None:
+        if update.message.reply_to_message==None and update.message.from_user.is_bot==False:
             test=test.replace(' #名言','').replace('#名言 ','')
             qlist=[test,update.message.from_user.first_name]
             work_sheet_push(qlist,'quote')
             return
     if test.find('#名言')!=-1:
-        if update.message.reply_to_message is not None:
+        if update.message.reply_to_message is not None and update.message.reply_to_message.from_user.is_bot==False:
             qlist=[update.message.reply_to_message.text,update.message.reply_to_message.from_user.first_name]
             work_sheet_push(qlist,'quote')
             return
