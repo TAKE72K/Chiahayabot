3662b31a728a8513dbb3977e2539970d7a226657
TAKE72K <hyes941073@gmail.com>
2018-07-31 14:48:52 +0800

    no slwwp

--- 72botpub.py
+++ 72botpub.py
@@ -595,6 +595,8 @@ def quote(bot,update):
     quote=qsheet.get_all_values()
     num=random.randint(0,len(quote)-1)
     text='<pre>'+quote[num][0]+'</pre>\n'+'-----<b>'+quote[num][1]+'</b> より'
+    par=random.randint(0,3)
+    time.sleep(par)
     msg=bot.send_message(chat_id=update.message.chat_id,text=text,parse_mode='HTML')
     if update.message.chat_id==-313454366:
         work_sheet_push([str(update.message.chat_id),str(msg.message_id)],'del')
