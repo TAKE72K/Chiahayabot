4f98a4ca8d5cb3b651405eedac7373cc8d09af8a
TAKE72K <hyes941073@gmail.com>
2018-08-12 23:30:48 +0800

    rc

--- 72botpub.py
+++ 72botpub.py
@@ -1,9 +1,6 @@
 ﻿import telegram
 import re
 import random
-from nicodic import summary
-from bs4 import BeautifulSoup
-from selenium import webdriver
 from random import randrange
 import os
 import sys
@@ -834,7 +831,7 @@ def sora(bot,update):
                 r=r.replace('n(','').replace(');','')
                 dicc=json.loads(r)
                 bot.send_message(chat_id=update.message.chat_id, text=dicc['summary'])
-                bot.send_message(chat_id=update.message.chat_id, text=summary(test))
+                #bot.send_message(chat_id=update.message.chat_id, text=summary(test))
             
             return
 
