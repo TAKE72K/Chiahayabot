71ae650da3f5882328bcc7bf35a431aa8c95a83d
TAKE72K <hyes941073@gmail.com>
2018-08-14 13:59:06 +0800

    link

--- 72botpub.py
+++ 72botpub.py
@@ -276,11 +276,11 @@ def start(bot, update):
     bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
     bot.send_message(chat_id=update.message.chat_id, text="如月千早です。劇場という場所があることは、レッスンの励みにもなりますね。これからも、厳しいご指導をよろしくお願いします。")
     bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
-    bot.send_message(chat_id=update.message.chat_id, text='私のコマンドリストです：\n/start-名為72的偶像\n/help-72能做什麼?\n/time-現在幾點\n/gdmn-早安\n/set-set早安名\n/kenka-吵架\n/grave-擔當太尊而猝死的P用\n/quote-千早歌詞集\n/bomb-自爆吧P\n/count-test function count members\n/dice-N粒公正的骰子(N<1000)\n/water-即時水量\n/about-關於此bot')
-
+    #bot.send_message(chat_id=update.message.chat_id, text='私のコマンドリストです：\n/start-名為72的偶像\n/help-72能做什麼?\n/time-現在幾點\n/gdmn-早安\n/set-set早安名\n/kenka-吵架\n/grave-擔當太尊而猝死的P用\n/quote-千早歌詞集\n/bomb-自爆吧P\n/count-test function count members\n/dice-N粒公正的骰子(N<1000)\n/water-即時水量\n/about-關於此bot')
+    bot.send_message(chat_id=update.message.chat_id, text="気軽にお声をおかけください～～　/help")
     button_list=[
-        InlineKeyboardButton(text='start',url='https://telegram.me/Chiahayabot?start=sticker'),
-        InlineKeyboardButton(text='about 72',url='https://imasml-theater-wiki.gamerch.com/%E5%A6%82%E6%9C%88%E5%8D%83%E6%97%A9#content_2_13')
+        InlineKeyboardButton(text='start me in PM',url='https://telegram.me/Chiahayabot?start=sticker'),
+        InlineKeyboardButton(text='about Chihaya.K',url='https://imasml-theater-wiki.gamerch.com/%E5%A6%82%E6%9C%88%E5%8D%83%E6%97%A9#content_2_13')
         ]
     
     reply_markup = InlineKeyboardMarkup(build_menu(button_list, n_cols=1))
