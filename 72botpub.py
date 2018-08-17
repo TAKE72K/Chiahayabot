f54e15fd002a3ef7e1c4413ed557165e5f633453
TAKE72K <hyes941073@gmail.com>
2018-08-17 17:49:20 +0800

    command

--- 72botpub.py
+++ 72botpub.py
@@ -57,6 +57,8 @@ quote-千早歌詞集
 bomb-自爆吧P
 count-test function count members
 dice-N粒公正的骰子(N<1000)
+gasya-一抽入魂
+sticker-アイマス鯖STICKERまとめ
 water-即時水量
 about-關於此bot
 '''
@@ -317,7 +319,24 @@ def start(bot, update,args):
 
 def help(bot,update):
     bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
-    bot.send_message(chat_id=update.message.chat_id, text='私のコマンドリストです：\n/start-名為72的偶像\n/help-72能做什麼?\n/time-現在幾點\n/gdmn-早安\n/set-set早安名\n/kenka-吵架\n/grave-擔當太尊而猝死的P用\n/quote-千早歌詞集\n/bomb-自爆吧P\n/count-test function count members\n/dice-N粒公正的骰子(N<1000)\n/water-即時水量\n/about-關於此bot')
+    bot.send_message(chat_id=update.message.chat_id, text="""
+    私のコマンドリストです：
+/start-名為72的偶像
+/help-72能做什麼?
+/time-現在幾點
+/gdmn-早安
+/set-set早安名
+/kenka-吵架
+/grave-擔當太尊而猝死的P用
+/quote-千早歌詞集
+/bomb-自爆吧P
+/count-test function count members
+/dice-N粒公正的骰子(N<1000)
+/gasya-一抽入魂
+/sticker-アイマス鯖STICKERまとめ
+/water-即時水量
+/about-關於此bot
+    """)
 
 def about(bot,update):
     text_a='''
