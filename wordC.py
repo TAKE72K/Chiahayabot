03ac46e3790a87db613871dce1b45f3c51ca9274
TAKE72K <33437633+TAKE72K@users.noreply.github.com>
2018-08-24 11:54:16 +0800

    Add files via upload

---
+++ wordC.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+from wordcloud import WordCloud as wc
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+spreadsheet_key=os.environ['SPREAD']
+def cloud(which,background_color='MidnightBlue',colormap='Wistia'):
+    scope = ['https://spreadsheets.google.com/feeds']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('auth.json', scope)
+    client = gspread.authorize(creds)
+    sheet = client.open_by_key(spreadsheet_key)
+    worksheet=sheet.worksheet(which)
+    content=worksheet.get_all_values()
+    text=''.join(i for i in content)
+    wcloud=wc(font_path='NotoSerifCJKtc-Regular.otf',
+        width=1920,height=1080,
+        relative_scaling=0.7,
+        background_color=background_color,colormap=colormap
+    )
+    image=wcloud.generate(text).to_image()
+    image.save('wc.jpg')
