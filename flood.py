d61c7e149bcafc2b789e408c9b6b50e80f0a42d9
TAKE72K <hyes941073@gmail.com>
2018-10-16 23:55:53 +0800

    clear buff

--- flood.py
+++ flood.py
@@ -56,5 +56,6 @@ class FloodLimit:
             can_send_messages=False, can_send_media_messages=False,
             can_send_other_messages=False)
             bot.send_message(chat_id=self.chatId, text=str(self.userId)+'閉嘴')
+            self.messageSet=[msgTop]
             return True
         return False
