c5038cd2727366e3a5a34c53a3861ee5973f04d6
Your Name <hyes941073@gmail.com>
2019-04-12 23:56:42 +0800

    del

--- flood.py
+++ flood.py
@@ -70,7 +70,7 @@ class FloodLimit:
             until_date=dt.now()+td(0,self.restrictT,0),
             can_send_messages=False, can_send_media_messages=False,
             can_send_other_messages=False)
-            bot.send_message(chat_id=self.chatId, text=self.userName+'閉嘴\n秒速'+"{0:.2f}".format(userF)+'則訊息，很快嘛ㄏㄏ，人造ファイヤファイボワイパー、タイガータイガータタタタタイガー、チャペアペカラキナ、チャペアペカラキナ、ミョーホントゥスケ   \nワイパー、ファイヤーファイヤー虎虎カラキナ、チャペアペファーマー海女海女ジャスパー、虎タイガー、虎タイガー、人造繊維、イエッタイガー')
+            bot.send_message(chat_id=self.chatId, text=self.userName+'閉嘴\n秒速'+"{0:.2f}".format(userF)+'則訊息，很快嘛ㄏㄏ，不就好棒棒，很會洗')
             for i in self.messageSet:
                 bot.delete_message(chat_id=self.chatId, message_id=i["msgId"])
             self.messageSet=[msgTop]
