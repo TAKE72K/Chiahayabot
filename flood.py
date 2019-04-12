f5d5e99ff306d62b0214415316d04f8b9f0f2971
Your Name <hyes941073@gmail.com>
2019-04-12 23:38:34 +0800

    del

--- flood.py
+++ flood.py
@@ -70,7 +70,8 @@ class FloodLimit:
             can_send_messages=False, can_send_media_messages=False,
             can_send_other_messages=False)
             bot.send_message(chat_id=self.chatId, text=self.userName+'閉嘴\n秒速'+"{0:.2f}".format(userF)+'則訊息，很快嘛ㄏㄏ')
-
+            for i in self.messageSet:
+                bot.delete_message(chat_id=self.chatId, message_id=i["msgId"])
             self.messageSet=[msgTop]
             return True
         return False
